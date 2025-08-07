f254343a08b07c11ffe5319eeb83c9e11f968255
MALEK-developer <malekmohammadhusenm@gmail.com>
2025-08-07 12:28:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,257 @@
+let keys = document.querySelectorAll('.key_btn');
+
+document.addEventListener('keyup', () => {
+
+    keys.forEach((key) => {
+        key.style.backgroundColor = 'white';
+        key.style.color = 'black';
+        key.style.boxShadow = '0px 0px 0px black';
+        key.textContent = key.textContent.toLowerCase();
+    });
+    if (event.key === 'a') {
+        document.querySelector('.key_btn1').style.backgroundColor = 'black';
+        document.querySelector('.key_btn1').style.color = 'white';
+        document.querySelector('.key_btn1').style.boxShadow = '0px 0px 10px black';
+        // alert('Entered Key is a.');
+    }
+
+    else if (event.key === 'b') {
+        document.querySelector('.key_btn2').style.backgroundColor = 'black';
+        document.querySelector('.key_btn2').style.color = 'white';
+        document.querySelector('.key_btn2').style.boxShadow = '0px 0px 10px black';
+        // alert('Entered Key is b.');
+    }
+
+    else if (event.key === 'c') {
+        document.querySelector('.key_btn3').style.backgroundColor = 'black';
+        document.querySelector('.key_btn3').style.color = 'white';
+        document.querySelector('.key_btn3').style.boxShadow = '0px 0px 10px black';
+        // alert('Entered Key is c.');
+    }
+
+    else if (event.key === 'd') {
+        document.querySelector('.key_btn4').style.backgroundColor = 'black';
+        document.querySelector('.key_btn4').style.color = 'white';
+        document.querySelector('.key_btn4').style.boxShadow = '0px 0px 10px black';
+        // alert('Entered Key is d.');
+    }
+
+    else if (event.key === 'e') {
+        document.querySelector('.key_btn5').style.backgroundColor = 'black';
+        document.querySelector('.key_btn5').style.color = 'white';
+        document.querySelector('.key_btn5').style.boxShadow = '0px 0px 10px black';
+        // alert('Entered Key is e.');
+    }
+
+    else if (event.key === 'f') {
+        document.querySelector('.key_btn6').style.backgroundColor = 'black';
+        document.querySelector('.key_btn6').style.color = 'white';
+        document.querySelector('.key_btn6').style.boxShadow = '0px 0px 10px black';
+        // alert('Entered Key is f.');
+    }
+
+    else if (event.key === 'g') {
+        document.querySelector('.key_btn7').style.backgroundColor = 'black';
+        document.querySelector('.key_btn7').style.color = 'white';
+        document.querySelector('.key_btn7').style.boxShadow = '0px 0px 10px black';
+        // alert('Entered Key is g.');
+    }
+
+    else if (event.key === 'h') {
+        document.querySelector('.key_btn8').style.backgroundColor = 'black';
+        document.querySelector('.key_btn8').style.color = 'white';
+        document.querySelector('.key_btn8').style.boxShadow = '0px 0px 10px black';
+        // alert('Entered Key is h.');
+    }
+
+    else if (event.key === 'i') {
+        document.querySelector('.key_btn9').style.backgroundColor = 'black';
+        document.querySelector('.key_btn9').style.color = 'white';
+        document.querySelector('.key_btn9').style.boxShadow = '0px 0px 10px black';
+        // alert('Entered Key is i.');
+    }
+
+    else if (event.key === 'j') {
+        document.querySelector('.key_btn10').style.backgroundColor = 'black';
+        document.querySelector('.key_btn10').style.color = 'white';
+        document.querySelector('.key_btn10').style.boxShadow = '0px 0px 10px black';
+        // alert('Entered Key is j.');
+    }
+
+    else if (event.key === 'k') {
+        document.querySelector('.key_btn11').style.backgroundColor = 'black';
+        document.querySelector('.key_btn11').style.color = 'white';
+        document.querySelector('.key_btn11').style.boxShadow = '0px 0px 10px black';
+        // alert('Entered Key is k.');
+    }
+
+    else if (event.key === 'l') {
+        document.querySelector('.key_btn12').style.backgroundColor = 'black';
+        document.querySelector('.key_btn12').style.color = 'white';
+        document.querySelector('.key_btn12').style.boxShadow = '0px 0px 10px black';
+        // alert('Entered Key is l.');
+    }
+
+    else if (event.key === 'm') {
+        document.querySelector('.key_btn13').style.backgroundColor = 'black';
+        document.querySelector('.key_btn13').style.color = 'white';
+        document.querySelector('.key_btn13').style.boxShadow = '0px 0px 10px black';
+        // alert('Entered Key is m.');
+    }
+
+    else if (event.key === 'n') {
+        document.querySelector('.key_btn14').style.backgroundColor = 'black';
+        document.querySelector('.key_btn14').style.color = 'white';
+        document.querySelector('.key_btn14').style.boxShadow = '0px 0px 10px black';
+        // alert('Entered Key is n.');
+    }
+
+    else if (event.key === 'o') {
+        document.querySelector('.key_btn15').style.backgroundColor = 'black';
+        document.querySelector('.key_btn15').style.color = 'white';
+        document.querySelector('.key_btn15').style.boxShadow = '0px 0px 10px black';
+        // alert('Entered Key is o.');
+    }
+
+    else if (event.key === 'p') {
+        document.querySelector('.key_btn16').style.backgroundColor = 'black';
+        document.querySelector('.key_btn16').style.color = 'white';
+        document.querySelector('.key_btn16').style.boxShadow = '0px 0px 10px black';
+        // alert('Entered Key is p.');
+    }
+
+    else if (event.key === 'q') {
+        document.querySelector('.key_btn17').style.backgroundColor = 'black';
+        document.querySelector('.key_btn17').style.color = 'white';
+        document.querySelector('.key_btn17').style.boxShadow = '0px 0px 10px black';
+        // alert('Entered Key is q.');
+    }
+
+    else if (event.key === 'r') {
+        document.querySelector('.key_btn18').style.backgroundColor = 'black';
+        document.querySelector('.key_btn18').style.color = 'white';
+        document.querySelector('.key_btn18').style.boxShadow = '0px 0px 10px black';
+        // alert('Entered Key is r.');
+    }
+
+    else if (event.key === 's') {
+        document.querySelector('.key_btn19').style.backgroundColor = 'black';
+        document.querySelector('.key_btn19').style.color = 'white';
+        document.querySelector('.key_btn19').style.boxShadow = '0px 0px 10px black';
+        // alert('Entered Key is s.');
+    }
+
+    else if (event.key === 't') {
+        document.querySelector('.key_btn20').style.backgroundColor = 'black';
+        document.querySelector('.key_btn20').style.color = 'white';
+        document.querySelector('.key_btn20').style.boxShadow = '0px 0px 10px black';
+        // alert('Entered Key is t.');
+    }
+
+    else if (event.key === 'u') {
+        document.querySelector('.key_btn21').style.backgroundColor = 'black';
+        document.querySelector('.key_btn21').style.color = 'white';
+        document.querySelector('.key_btn21').style.boxShadow = '0px 0px 10px black';
+        // alert('Entered Key is u.');
+    }
+
+    else if (event.key === 'v') {
+        document.querySelector('.key_btn22').style.backgroundColor = 'black';
+        document.querySelector('.key_btn22').style.color = 'white';
+        document.querySelector('.key_btn22').style.boxShadow = '0px 0px 10px black';
+        // alert('Entered Key is v.');
+    }
+
+    else if (event.key === 'w') {
+        document.querySelector('.key_btn23').style.backgroundColor = 'black';
+        document.querySelector('.key_btn23').style.color = 'white';
+        document.querySelector('.key_btn23').style.boxShadow = '0px 0px 10px black';
+        // alert('Entered Key is w.');
+    }
+
+    else if (event.key === 'x') {
+        document.querySelector('.key_btn24').style.backgroundColor = 'black';
+        document.querySelector('.key_btn24').style.color = 'white';
+        document.querySelector('.key_btn24').style.boxShadow = '0px 0px 10px black';
+        // alert('Entered Key is x.');
+    }
+
+    else if (event.key === 'y') {
+        document.querySelector('.key_btn25').style.backgroundColor = 'black';
+        document.querySelector('.key_btn25').style.color = 'white';
+        document.querySelector('.key_btn25').style.boxShadow = '0px 0px 10px black';
+        // alert('Entered Key is y.');
+    }
+
+    else if (event.key === 'z') {
+        document.querySelector('.key_btn26').style.backgroundColor = 'black';
+        document.querySelector('.key_btn26').style.color = 'white';
+        document.querySelector('.key_btn26').style.boxShadow = '0px 0px 10px black';
+        // alert('Entered Key is z.');
+    }
+
+    else if (event.key === 'Control') {
+        document.querySelector('.key_btn27').style.backgroundColor = 'black';
+        document.querySelector('.key_btn27').style.color = 'white';
+        document.querySelector('.key_btn27').style.boxShadow = '0px 0px 10px black';
+        // alert('Entered Key is Control.');
+    }
+
+    else if (event.key === 'Alt') {
+        document.querySelector('.key_btn28').style.backgroundColor = 'black';
+        document.querySelector('.key_btn28').style.color = 'white';
+        document.querySelector('.key_btn28').style.boxShadow = '0px 0px 10px black';
+        // alert('Entered Key is Alt.');
+    }
+
+    else if (event.key === 'Shift') {
+        document.querySelector('.key_btn29').style.backgroundColor = 'black';
+        document.querySelector('.key_btn29').style.color = 'white';
+        document.querySelector('.key_btn29').style.boxShadow = '0px 0px 10px black';
+        // alert('Entered Key is Shift.');
+        keys.forEach((new_key) => {
+            new_key.textContent = new_key.textContent.toUpperCase();
+        });
+    }
+
+    else if (event.key === 'Tab') {
+        document.querySelector('.key_btn30').style.backgroundColor = 'black';
+        document.querySelector('.key_btn30').style.color = 'white';
+        document.querySelector('.key_btn30').style.boxShadow = '0px 0px 10px black';
+        // alert('Entered Key is Tab.');
+    }
+
+    else if (event.key === ' ') {
+        document.querySelector('.key_btn31').style.backgroundColor = 'black';
+        document.querySelector('.key_btn31').style.color = 'white';
+        document.querySelector('.key_btn31').style.boxShadow = '0px 0px 10px black';
+        // alert('Entered Key is  .');
+    }
+
+    else if (event.key === 'Escape') {
+        document.querySelector('.key_btn32').style.backgroundColor = 'black';
+        document.querySelector('.key_btn32').style.color = 'white';
+        document.querySelector('.key_btn32').style.boxShadow = '0px 0px 10px black';
+        // alert('Entered Key is Escape.');
+    }
+
+    else if (event.key === 'Backspace') {
+        document.querySelector('.key_btn33').style.backgroundColor = 'black';
+        document.querySelector('.key_btn33').style.color = 'white';
+        document.querySelector('.key_btn33').style.boxShadow = '0px 0px 10px black';
+        // alert('Entered Key is Backspace.');
+    }
+
+    else if (event.key === 'Enter') {
+        document.querySelector('.key_btn34').style.backgroundColor = 'black';
+        document.querySelector('.key_btn34').style.color = 'white';
+        document.querySelector('.key_btn34').style.boxShadow = '0px 0px 10px black';
+        // alert('Entered Key is Enter.');
+    }
+
+    else {
+        document.querySelector('.key_btn').style.backgroundColor = 'black';
+        document.querySelector('.key_btn').style.color = 'white';
+        document.querySelector('.key_btn').style.boxShadow = '0px 0px 10px black';
+    }
+});
